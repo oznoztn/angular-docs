aac6c933ce242c81960d524573e68e417576dd56
oznoztn <goldenhawkk@gmail.com>
2020-02-14 23:41:24 +0300

    Rename 'Hero' to 'Character' across all the relevant files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms"; // <-- NgModel lives here
 
-import { AppComponent } from './app.component';
-import { HeroesComponent } from './heroes/heroes.component';
+import { AppComponent } from "./app.component";
+import { CharacterListComponent } from "./character-list/character-list.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeroesComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule
-  ],
+  declarations: [AppComponent, CharacterListComponent],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/character-list/character-list.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from "@angular/core";
+import { Character } from "../character";
+import { CHARACTERS } from "../mock-characters";
+
+@Component({
+  selector: "app-character-list",
+  templateUrl: "./character-list.component.html",
+  styleUrls: ["./character-list.component.css"]
+})
+export class CharacterListComponent implements OnInit {
+  characters = CHARACTERS;
+  selectedCharacter: Character;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onSelect(character) {
+    this.selectedCharacter = character;
+  }
+}
