d4eefa22add676d9ef28555ee5e8ecde23185136
oznoztn <goldenhawkk@gmail.com>
2020-02-16 15:50:20 +0300

    Render a Character com for each char --@Input()

--- src/app/character/character.component.ts
+++ src/app/character/character.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Character } from "../character";
+import { Input } from "@angular/core";
 
 @Component({
-  selector: 'app-character',
-  templateUrl: './character.component.html',
-  styleUrls: ['./character.component.css']
+  selector: "app-character",
+  templateUrl: "./character.component.html",
+  styleUrls: ["./character.component.css"]
 })
 export class CharacterComponent implements OnInit {
+  @Input() char: Character;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
