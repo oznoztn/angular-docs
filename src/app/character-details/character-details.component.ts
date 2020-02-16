f30667f06fcadf93b26293b380db3a97c6b04b75
oznoztn <goldenhawkk@gmail.com>
2020-02-16 17:37:20 +0300

    Move character display template into Char. comp.

--- src/app/character-details/character-details.component.ts
+++ src/app/character-details/character-details.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from "@angular/core";
+import { Character } from "../character";
 
 @Component({
-  selector: 'app-character-details',
-  templateUrl: './character-details.component.html',
-  styleUrls: ['./character-details.component.css']
+  selector: "app-character-details",
+  templateUrl: "./character-details.component.html",
+  styleUrls: ["./character-details.component.css"]
 })
 export class CharacterDetailsComponent implements OnInit {
+  @Input() character: Character;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
