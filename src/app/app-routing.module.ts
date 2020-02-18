db716aa02d52ded1b3660b73cfbbe84399e8f096
oznoztn <goldenhawkk@gmail.com>
2020-02-18 15:34:31 +0300

    Add route definition for MainScreenComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CharacterListComponent } from './character-list/character-list.component';
+import { MainScreenComponent } from './main-screen/main-screen.component';
 
 const routes: Routes = [
+  { path: '', component: MainScreenComponent },
   { path: 'characters', component: CharacterListComponent }
 ];
 
