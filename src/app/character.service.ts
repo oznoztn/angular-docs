ac823831d895e71151b0881de91b179d48b8d253
oznoztn <goldenhawkk@gmail.com>
2020-02-18 11:45:00 +0300

    Add getCharacters method that returns mock data

--- src/app/character.service.ts
+++ src/app/character.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
+import { CHARACTERS } from './mock-characters';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CharacterService {
-
   constructor() { }
+
+  getCharacters() {
+    return CHARACTERS;
+  }
 }
