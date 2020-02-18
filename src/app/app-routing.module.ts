eaab9f7bdb73c1a505bcd31671ed44fd14d5e22d
oznoztn <goldenhawkk@gmail.com>
2020-02-18 16:44:47 +0300

    Add route definition for CharacterDetails component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CharacterListComponent } from './character-list/character-list.component';
 import { MainScreenComponent } from './main-screen/main-screen.component';
+import { CharacterDetailsComponent } from './character-details/character-details.component';
 
 const routes: Routes = [
   { path: '', component: MainScreenComponent, pathMatch: 'full' },
-  { path: 'characters', component: CharacterListComponent }
+  { path: 'characters', component: CharacterListComponent },
+  { path: 'details/:characterId', component: CharacterDetailsComponent}
 ];
 
 @NgModule({
