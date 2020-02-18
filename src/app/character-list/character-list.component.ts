cf07423130e204f0cfa851dfebfac825ba185c2d
oznoztn <goldenhawkk@gmail.com>
2020-02-18 11:59:35 +0300

    Add getCharacters func to retrive & set char. data

--- src/app/character-list/character-list.component.ts
+++ src/app/character-list/character-list.component.ts
@@ -15,12 +15,22 @@ export class CharacterListComponent implements OnInit {
     // Dependency injection is in action here
     private characterService: CharacterService
   ) {
-    this.characters = this.characterService.getCharacters();
+    this.getCharacters();
   }
 
   ngOnInit(): void {}
 
   onSelect(character) {
     this.selectedCharacter = character;
+  }
+
+// While you could call getHeroes() in the constructor, that's not the best practice.
+// Reserve the constructor for simple initialization such as wiring constructor parameters to properties. 
+// The constructor shouldn't do anything. 
+// It certainly shouldn't call a function that makes HTTP requests to a remote server as a real data service would.
+// Instead, call getHeroes() inside the ngOnInit lifecycle hook 
+//   and let Angular call ngOnInit() at an appropriate time after constructing a HeroesComponent instance.
+  getCharacters(): void {
+    this.characters = this.characterService.getCharacters();
   }
 }
